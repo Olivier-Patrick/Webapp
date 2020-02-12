d08567436c9ae2e5a7315aad0030e9c7d4193d82
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-12 17:11:30 +0100

    message

--- resources/views/template_category/header.blade.php
+++ resources/views/template_category/header.blade.php
@@ -1,48 +1,50 @@
 
 
 <body>
- <nav id="menuprincipal" class="navbar navbar-default navbar-fixed-top">
-    <div class="flex items-center justify-center flex-wrap bg-gray-200 p-4">          
-        <div class="navbar-header">           
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-                <span class="sr-only">Menu</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>            
-        <div id="navbar" class="collapse navbar-collapse" >
-            <ul class="nav navbar-nav">
-                <li class="flex items-center flex-shrink-0 mr-2">                      
-                  <a href="/" class="d-flex">
-                    <img style="width:60px; max-width:60px" src="{{asset('img/logo.png')}}" alt="">
-                  </a>
-                </li>
-                <li class="mr-12" ><a href="/" style="font-size:20px" class="font-semibold text-black-500 tracking-tight">Toutes vos annonce gratuites</a></li>
-                <li class="mr-3"><a href="/post" style="font-size:18px" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">Publier une annonce</a></li>
-                <li class="mr-3"><a href="faq" style="font-size:18px" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded">Aide/FAQ</a></li>
-                <li class="dropdown">
-                    <a href="#" style="font-size:18px" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ma Jamula<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/majamula">Mes annonces</a></li>
-                        <li><a href="#">Messages</a></li>
-                        <li><a href="#">Achats</a></li>
-                        <li><a href="#">Profil</a></li>
-                        <li><a href="#">Deconnexion</a></li>
-                    </ul>
-                </li>
-                <li><a href="/login" style="font-size:18px" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-green-500 hover:border-transparent rounded">Se connecter/Creer mon compte</a></li>
-                <li class="dropdown mr-12">
-                    <a href="#" style="font-size:18px" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pays<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Maroc</a></li>
-                        <li><a href="#">Côte d'Ivoire</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
+<nav class="flex items-center justify-between flex-wrap bg-gray-200 p-4">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <a href="/">
+                <img style="width:80px; max-width:80px" src="{{asset('img/logo.png')}}" alt="">
+            </a>
+            <span class="font-semibold text-black tracking-tight">Toutes vos annonce gratuites</span>
     </div>
- </nav> 
+            <!-- <div class="block lg:hidden">
+               <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+               </button>
+            </div> -->
+    <div>
+        <div class="text-lg flex "> 
+            <div>
+                <a href="{{route('post.create_post')}}" class="bg-red-600 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded-full mr-3">
+                    Publier une annone
+                </a>
+            </div>
+            <div>
+                <a href="faq" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-green-500 mr-2">
+                    Aide/FAQ
+                </a>
+            </div>
+                      
+            <div class="inline-block relative w-64 mr-2">
+                <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-6 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                    <option><a href="#"> Mon Berenie</a> </option>
+                    <option><a href="#">Mon annonce</a> </option>
+                    <option><a href="#">Message</a> </option>
+                    <option><a href="#">Achats</a> </option>
+                </select>                     
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+            </div>
+            <div>
+                <a href="/login" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-green-500">
+                    Se connecter/Creer mon compte 
+                </a>
+            </div>
+        </div>
+    </div>    
+ </nav>
 
  <div class="bg-cover bg-center " style="background-image: url(http://www.mobot.org/MOBOT/research/vegmad/images/637_028l.gif)" class="header">
     <br>
