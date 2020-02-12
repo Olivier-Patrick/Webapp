1b3b4eb948ba4ed26a1d2fc82489215e8507d383
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-12 16:10:55 +0100

    message

---
+++ resources/views/front/categorie/communaute/communaute.blade.php
@@ -0,0 +1,144 @@
+@extends('template_category.home')
+@section('content') 
+<div class="container">
+  <div class="breadcrumb__container flex justify ml-4">
+      <div class="breadcrumb content_container">
+         <div class="breadcrumb_new">
+            <a href="/" itemprop="url" class="breadcrumb_item" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+               <span itemprop="title">Toutes votre annonces gratuites ></span>
+            </a>
+             <a class="breadcrumb_item" title="" href="/communaute">communaute</a>                
+         </div>
+      </div>
+  </div> 
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+   <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+  <div class="post post-row">
+	<a class="post-img" href="blog-post.html"><img src="./img/post-13.jpg" alt=""></a>
+	<div class="post-body">
+		<div class="post-category">
+			<a href="category.html">Travel</a>
+			<a href="category.html">Lifestyle</a>
+		</div>
+		<h3 class="post-title"><a href="blog-post.html">Mel ut impetus suscipit tincidunt. Cum id ullum laboramus persequeris.</a></h3>
+		<ul class="post-meta">
+			<li><a href="author.html">John Doe</a></li>
+			<li>20 April 2018</li>
+		</ul>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...</p>
+	</div>
+  </div>
+
+
+  <br>
+  <div>  
+    <div class="ml-4">
+      <strong>Communauté pour toutes vos annonces gratuites</strong><br>
+      <p>La catégorie Communauté est, comme son nom l’indique, faite pour mettre les utilisateurs de Locanto Maroc en relation entre eux : organisez des activités, rendez-vous des petits services, trouvez des équipiers ou des partenaires...</p><br>
+      <p>Si vous souhaitez apporter votre contribution à la communauté jamulapub, que ce soit une offre de baby sitter, proposez vos petits services aux particuliers quels qu’ils soient,
+         <a href="/post" class="text-blue-800">publiez votre annonce sur jamula</a>  sans plus attendre ! C’est gratuit !</p>
+    </div>
+      <br>  
+      <br>
+  </div>
+</div>
+
+@endsection
+	
+					
+
