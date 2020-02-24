d22724e01dd343e5067499d2504284ec11fdccd3
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-24 16:37:19 +0100

    message

--- resources/views/webapp.blade.php
+++ resources/views/webapp.blade.php
@@ -226,70 +226,70 @@
     				<div class="category-wrap">
     					<div class="row no-gutters">
     						<div class="col-md-1" >
-    							<div class="top-category text-center no-border-left" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center no-border-left" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Website &amp; Software</a></h3>
     								<span class="icon flaticon-contact"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
     						<div class="col-md-1" >
-    							<div class="top-category text-center active" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center active" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Education &amp; Training</a></h3>
     								<span class="icon flaticon-mortarboard"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
     						<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Graphic &amp; UI/UX Design</a></h3>
     								<span class="icon flaticon-idea"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
     						<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Accounting &amp; Finance</a></h3>
     								<span class="icon flaticon-accounting"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
     						<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Restaurant &amp; Food</a></h3>
     								<span class="icon flaticon-dish"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
     						<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Health &amp; Hospital</a></h3>
     								<span class="icon flaticon-stethoscope"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
 							<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Health &amp; Hospital</a></h3>
     								<span class="icon flaticon-stethoscope"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
 							<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Health &amp; Hospital</a></h3>
     								<span class="icon flaticon-stethoscope"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
 							<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Health &amp; Hospital</a></h3>
     								<span class="icon flaticon-stethoscope"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
     							</div>
     						</div>
 							<div class="col-md-1">
-    							<div class="top-category text-center" style="max-width : 70px; heigth : 20px ">
+    							<div class="top-category text-center" style="width : 70px; heigth : 20px ">
     								<h3><a href="#">Health &amp; Hospital</a></h3>
     								<span class="icon flaticon-stethoscope"></span>
     								<p><span class="number">143</span> <span>Open position</span></p>
