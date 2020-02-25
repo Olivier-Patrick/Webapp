b246bd21f2a736f69a520d131c653db3db9a0f96
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-25 11:27:23 +0100

    message

--- resources/views/webapp.blade.php
+++ resources/views/webapp.blade.php
@@ -50,7 +50,7 @@
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-7">
-                                <div class="main-menu  d-lg-block">
+                                <div class="main-menu  d-none d-lg-block">
                                     <nav>
                                         <ul id="navigation">
                                             <li><a href="index.html">home</a></li>
@@ -70,7 +70,7 @@
                                                 </ul>
 											</li>
 											<li><a href="index.html" class="genric-btn danger-boder radius">je me connecte</a></li>
-											<li><a href="index.html">je m'inscris</a></li>
+											<li><a href="index.html" class="genric-btn danger-boder radius">je m'inscris</a></li>
 											
                                         </ul>
                                     </nav>
