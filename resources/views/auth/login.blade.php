2c25270648974c0bc1939e2ca190e952ea54f959
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-26 01:37:30 +0100

    message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ assets('assets2/images/icons/favicon.ico')}}"/>
+	<link rel="icon" type="image/png" href="{{ asset('assets2/images/icons/favicon.ico')}}"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/bootstrap/css/bootstrap.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/fonts/iconic/css/material-design-iconic-font.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/animate/animate.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/animate/animate.css')}}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/css-hamburgers/hamburgers.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/css-hamburgers/hamburgers.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/animsition/css/animsition.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/animsition/css/animsition.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/select2/select2.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/select2/select2.min.css')}}">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/vendor/daterangepicker/daterangepicker.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/vendor/daterangepicker/daterangepicker.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/css/util.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ assets('assets2/css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets2/css/main.css')}}">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -103,21 +103,21 @@
 	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/animsition/js/animsition.min.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/animsition/js/animsition.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/bootstrap/js/popper.js')}}"></script>
-	<script src="{{ assets('assets2/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/select2/select2.min.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/select2/select2.min.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/daterangepicker/moment.min.js')}}"></script>
-	<script src="{{ assets('assets2/vendor/daterangepicker/daterangepicker.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/daterangepicker/moment.min.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/daterangepicker/daterangepicker.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/vendor/countdowntime/countdowntime.js')}}"></script>
+	<script src="{{ asset('assets2/vendor/countdowntime/countdowntime.js')}}"></script>
 <!--===============================================================================================-->
-	<script src="{{ assets('assets2/js/main.js')}}"></script>
+	<script src="{{ asset('assets2/js/main.js')}}"></script>
 
 </body>
 </html>
