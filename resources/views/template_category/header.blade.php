908efcd67cb8ab4a8ba9b817fb485420866c5ffc
Olivier-Patrick <gopatrik8@gmail.com>
2020-02-27 22:46:19 +0100

    message

--- resources/views/template_category/header.blade.php
+++ resources/views/template_category/header.blade.php
@@ -1,7 +1,7 @@
 
     <!-- header-start -->
     <header>
-        <div class="header-area ">
+        <div class="">
             <div id="sticky-header" class="main-header-area">
                 <div class="container-fluid ">
                     <div class="header_bottom_border">
